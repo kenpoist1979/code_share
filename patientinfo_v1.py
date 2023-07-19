55a1bd40bc828ca590dde1d9fb294d2da04033ce
kenpoist1979 <79804453+kenpoist1979@users.noreply.github.com>
2023-07-18 20:31:42 -0400

    Add files via upload
    Working on console input of patient data which outputs in JSON format and written to MySQL database on the  backend

---
+++ patientinfo_v1.py
@@ -0,0 +1,136 @@
+# from errno import errorcode
+# import random
+# import json
+# import mysql.connector
+# from tkinter import ttk as tK
+# 
+# db = mysql.connector.connect(host = "localhost", 
+                                # username = "admin@localhost", 
+                                # password = "R6!j33h9c6", 
+                                # database = "patient_info")
+# 
+# cur = db.cursor()
+# 
+# 
+# 
+# print("\n")
+# print("\n")
+# print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+# print("|Patient Information                                        |")
+# print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
+# 
+# medical_record_num = random.randint(10000,99999)
+# firstname = input("Patient First Name: ")
+# lastname = input("Patient Last Name: ")
+# address = input("Address: ")
+# dob = input("Enter Date of Birth: ")
+# serial_num = input("Enter Social Security: ")
+# doctor_name = input("Enter Doctor's Name: ")
+# 
+# print("**************************************\n")
+# print("**************************************\n")
+# 
+# q = "insert into patient_info values(%s,%s,%s,%s,%s,%s)"
+# data_input = (firstname,lastname,address,dob,serial_num,doctor_name)
+# cur.execute(q,data_input)
+# cur.execute("select * from patient_info")
+# results = cur.fetchall()
+# 
+# 
+# db.commit()
+# cur.close()
+# db.close()
+# 
+# for r in results:
+    # print(r)
+# 
+# 
+# print("**************************************\n")
+# print("**************************************\n")
+# 
+# patient_dict = {
+    # "medical_record_num": medical_record_num,
+    # "firstname":firstname,
+    # "lastname": lastname,
+    # "address":address,
+    # "dob":dob,
+    # "serial_num":serial_num,
+    # "doctor_name": doctor_name
+# }
+# 
+# converted_json = json.dumps(patient_dict,indent=2)
+# print(converted_json)
+
+import random
+import json
+import mysql.connector
+import getpass
+
+
+def main():
+    db = mysql.connector.connect(host = "localhost",
+                                username = "admin@localhost",
+                                password = getpass.getpass("Enter password: "),
+                                database = "hospital")
+    cur = db.cursor()
+
+
+
+
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print("|Patient Information                                        |")
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
+
+    medical_record_num = random.randint(10000,99999)
+    firstname = input("Patient First Name: ")
+    lastname = input("Patient Last Name: ")
+    address = input("Address: ")
+    dob = input("Enter Date of Birth: ")
+    serial_num = input("Enter Social Security: ")
+    doctor_name = input("Enter Doctor's Name: ")
+
+    print("**************************************\n")
+    print("**************************************\n")
+
+    quy = "insert into patient_info values(%s,%s,%s,%s,%s,%s,%s)"
+    data_input = (medical_record_num,firstname,lastname,address,dob,serial_num,doctor_name)
+    cur.execute(quy,data_input)
+    cur.execute("select * from patient_info")
+    results = cur.fetchall()
+
+
+    db.commit()
+    cur.close()
+    db.close()
+
+
+    patient_dict = {
+        "medical_record_num": medical_record_num,
+        "firstname":firstname,
+        "lastname": lastname,
+        "address":address,
+        "dob":dob,
+        "serial_num":serial_num,
+        "doctor_name": doctor_name
+        }
+
+    converted_json = json.dumps(patient_dict,indent=2)
+    print(converted_json)
+    
+    
+    print("**************************************\n")
+    print("**************************************\n")
+
+    for r in results:
+        print(r[0],' ',r[1],' ',r[2],' ',r[3],' ',r[4],' ',r[5],' ',r[6])
+
+
+
+
+
+if __name__ == "__main__":
+    while True:
+        main()
+        user_input = input("Enter 'q' to exit: ")
+        if user_input.lower() == 'q':
+            break
